825f6948ad825828bc1d346da3e565f196ac7f9e
DoGooder9000 <mike@rogness.net>
2023-11-22 19:58:13 -0600

    Added a Sprite, Collision, and Mouse Movement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from math import sin, cos, tan, radians
+from math import sin, cos, tan, atan2, radians, degrees, sqrt
 
 pygame.init()
 
@@ -9,18 +9,31 @@ def DrawMap(color: tuple):
 			if map[r][t] == "X":
 				pygame.draw.rect(window, color, pygame.Rect((t*tile_width, r*tile_height), (tile_width, tile_height)))
 
-def DrawPlayer(color: tuple, radius: float = 2):
-	pygame.draw.circle(window, color, PlayerPos, radius)
+def DrawPlayer():
+	window.blit(pygame.transform.rotate(GunGuySprite, -degrees(PlayerAngle)), (PlayerPos[0]-GunGuySize[0]/2, PlayerPos[1]-GunGuySize[1]/2))
+
+def GetTile(point: tuple):
+	try:
+		return map[int(point[1]/tile_height)][int(point[0]/tile_width)]
+	except IndexError:
+		return None
+
+def GetPoint(start_point: tuple, angle: float, length: float):
+	return (start_point[0] + cos(angle)*length, start_point[1] + sin(angle)*length)
+
+def Distance(p1, p2):
+	return sqrt((p2[0] - p1[0])**2 + (p2[1] - p1[1])**2)
 
 def Update():
 	pygame.display.update()
 
 
+window = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+
 window_size = (600, 800)
-window_width, window_height = window_size
+window_width, window_height = window.get_size()
 title = "Top Down Shooter"
 
-window = pygame.display.set_mode(window_size)
 pygame.display.set_caption(title)
 
 
@@ -28,7 +41,7 @@ PlayerPos = (window_width/2, window_height/2)
 PlayerAngle = 0
 
 PlayerSpeed = 1.5
-PlayerTurningSpeed = 1
+PlayerTurningSpeed = 2
 
 
 map = [
@@ -57,6 +70,9 @@ red = 	(255, 0, 0)
 green = (0, 255, 0)
 blue = 	(0, 0, 255)
 
+GunGuySize = (75, 75)
+GunGuySprite = pygame.transform.scale(pygame.image.load("TopDownGunGuy.png"), GunGuySize)
+
 clock = pygame.time.Clock()
 
 running = True
@@ -71,22 +87,29 @@ while running:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			running = False
+		
+		elif event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_ESCAPE:
+				running = False
+
+
+	mousePos = pygame.mouse.get_pos()
+	PlayerAngle = atan2(mousePos[1]-PlayerPos[1], mousePos[0]-PlayerPos[0])
+
 	
 	keys = pygame.key.get_pressed()
 
 	if keys[pygame.K_w]:
-		PlayerPos = (PlayerPos[0]+cos(PlayerAngle)*PlayerSpeed*dt, PlayerPos[1]+sin(PlayerAngle)*PlayerSpeed*dt)
+		NextPos = (PlayerPos[0]+cos(PlayerAngle)*PlayerSpeed*dt, PlayerPos[1]+sin(PlayerAngle)*PlayerSpeed*dt)
+		if GetTile(NextPos) != "X" and Distance(PlayerPos, mousePos) > 2: PlayerPos = NextPos
 	if keys[pygame.K_s]:
-		PlayerPos = (PlayerPos[0]-cos(PlayerAngle)*PlayerSpeed*dt, PlayerPos[1]-sin(PlayerAngle)*PlayerSpeed*dt)
-	if keys[pygame.K_a]:
-		PlayerAngle -= radians(PlayerTurningSpeed*dt)
-	if keys[pygame.K_d]:
-		PlayerAngle += radians(PlayerTurningSpeed*dt)
+		NextPosPos = (PlayerPos[0]-cos(PlayerAngle)*PlayerSpeed*dt, PlayerPos[1]-sin(PlayerAngle)*PlayerSpeed*dt)
+		if GetTile(NextPos) != "X" and Distance(PlayerPos, mousePos) > 2: PlayerPos = NextPos
 	
 	window.fill((100, 100, 100))
 
 	DrawMap(white)
 
-	DrawPlayer(blue)
+	DrawPlayer()
 	
 	Update()
